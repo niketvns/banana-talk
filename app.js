eabb129e49a3fa19e803bcf8957d0c115901ca38
NIKET KUMAR MISHRA <niketvns97@gmail.com>
2022-09-16 09:34:54 +0530

    docs: JavaScript file add

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 
+var fBody = document.querySelector("body");
 var translate = document.querySelector("#translate");
 
 var textInput = document.querySelector("#usr-input");
+
+var outText = document.querySelector("#outText");
 
 translate.addEventListener("click", eventHandler);
 
 function eventHandler() {
     console.log(textInput.value);
+    outText.innerHTML = textInput.value + " This is JavaScript Text";
+    fBody.style.backgroundColor = ""
 }
+
+
+
+
